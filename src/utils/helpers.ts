4560606496bf23341dbd7d58627b8216f904844d
Ken Yun <yunhaen@gmail.com>
2024-10-08 10:15:14 -0400

    feat: created helper function to convert fen string to board object

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,41 +1,43 @@
 import type { PieceSymbol, Color, Square } from 'chess.js'
-import {
-  PAWN,
-  ROOK,
-  KNIGHT,
-  BISHOP,
-  QUEEN,
-  KING,
-} from 'chess.js'
 
-export const getPieceIconURL = (type: PieceSymbol, color: Color) => {
-  switch (type) {
-    case PAWN:
-      return `/pieces/pawn-${color}.svg`;
-    case ROOK:
-      return `/pieces/rook-${color}.svg`;
-    case KNIGHT:
-      return `/pieces/knight-${color}.svg`;
-    case BISHOP:
-      return `/pieces/bishop-${color}.svg`;
-    case QUEEN:
-      return `/pieces/queen-${color}.svg`;
-    case KING:
-      return `/pieces/king-${color}.svg`;
-    default:
-      throw new Error('Invalid piece type');
+
+export type BoardCell = {
+  square: Square,
+  type: PieceSymbol,
+  color: Color,
+} | null;
+
+
+export const fenToBoard = (fen: string): Array<Array<BoardCell>> => {
+  const board: Array<Array<BoardCell>> = [];
+  const ranks = [8, 7, 6, 5, 4, 3, 2, 1];
+  const files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+  const notationParts = fen.split(' ');
+  const position = notationParts[0];
+  const fenRows = position.split('/');
+
+  for (let i = 0; i < 8; i++) {
+    let boardRow: Array<BoardCell> = [];
+    let fenRow = fenRows[i].split('');
+    for (let j = 0; j < 8; j++) {
+      if (Number(fenRow[j])) {
+        boardRow.push(...new Array(Number(fenRow[j])).fill(null) as Array<BoardCell>);
+        j += Number(fenRow[j]) - 1;
+      } else {
+        const square = `${files[j]}${ranks[i]}` as Square;
+        const type = fenRow[j].toLowerCase() as PieceSymbol;
+        const color = (fenRow[j].charCodeAt(0) >= 'a'.charCodeAt(0) ? 'b' : 'w') as Color;
+        boardRow.push({
+          square,
+          type,
+          color,
+        });
+      }
+    }
+
+    board.push(boardRow); 
   }
-}
 
-export const convertNotationToRowCol = (square: Square, isFlipped: boolean) => {
-  const [file, rank] = square.split('');
-
-  return {
-    row: isFlipped 
-      ? 'h'.charCodeAt(0) - file.charCodeAt(0) 
-      : file.charCodeAt(0) - 'a'.charCodeAt(0),
-    col: isFlipped 
-      ? Number(rank) - 1 
-      : 8 - Number(rank),
-  }
+  return board;
 }
