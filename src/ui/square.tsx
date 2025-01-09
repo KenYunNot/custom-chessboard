9edff2b78196a0313fe82dce2e383468ae62b639
Ken Yun <yunhaen@gmail.com>
2025-01-08 19:25:33 -0500

    feat: add outline when dragging

--- src/ui/square.tsx
+++ src/ui/square.tsx
@@ -15,17 +15,19 @@ const Square = ({
   
   children,
 }: SquareProps) => {
-  const { setNodeRef, over, isOver } = useDroppable({
+  const { setNodeRef, isOver } = useDroppable({
     id: `${row}-${col}`
   })
 
   return (
-    <div ref={setNodeRef} className={cn(`square ${8-row}${col+1}`, 
-      {
-        'bg-[#ebecd0]' : (row + col) % 2 === 0, // even squares are light square
-        'bg-[#749552]' : (row + col) % 2 === 1, // odd squares are dark squares
-      },
-    )}>
+    <div ref={setNodeRef} className={cn(`square ${8-row}${col+1}`, {
+      'bg-[#ebecd0]' : (row + col) % 2 === 0, // even squares are light square
+      'bg-[#749552]' : (row + col) % 2 === 1, // odd squares are dark squares
+    }, 
+    isOver && {
+      'shadow-[0_0_0_4px_inset_#d1d5db]' : (row + col) % 2 === 0, // even squares are light square
+      'shadow-[0_0_0_4px_inset_#e5e7eb]' : (row + col) % 2 === 1, // odd squares are dark squares
+    })}>
       {children}
     </div>
   )
