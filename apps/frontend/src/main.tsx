ea413a57658fa9aaa071b5d1a41295006151030d
Ken Yun <yunhaen@gmail.com>
2025-04-16 11:11:10 -0400

    feat: create routes for home, signin, and signup

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import { ClerkProvider } from '@clerk/clerk-react';
 import { BrowserRouter, Routes, Route } from 'react-router';
+import SignIn from './routes/signin';
+import SignUp from './routes/signup';
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -19,7 +21,21 @@ createRoot(document.getElementById('root')!).render(
       afterSignOutUrl='/'
     >
       <BrowserRouter>
-        <App />
+        <Routes>
+          <Route
+            path='/'
+            element={<App />}
+          />
+
+          <Route
+            path='signin'
+            element={<SignIn />}
+          />
+          <Route
+            path='signup'
+            element={<SignUp />}
+          />
+        </Routes>
       </BrowserRouter>
     </ClerkProvider>
   </StrictMode>
