2de73b56707f898c309a79fd6d685dfffd5879a8
Ken Yun <yunhaen@gmail.com>
2025-04-16 10:49:00 -0400

    feat: simple socket.io setup

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,10 +1,24 @@
 import express from 'express';
-const app = express();
+import { createServer } from 'node:http';
+import { join } from 'node:path';
+import { Server } from 'socket.io';
 
-app.get('/', (_, res) => {
-  res.send('Hello world!');
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: ['http://localhost:5173', 'http://localhost:4173'],
+  },
 });
 
-app.listen(3000, () => {
+app.get('/', (_, res) => {
+  res.sendFile(join(__dirname, '../', 'index.html'));
+});
+
+io.on('connection', (_) => {
+  console.log('A user connected!');
+});
+
+server.listen(3000, () => {
   console.log('Server listening on port 3000...');
 });
