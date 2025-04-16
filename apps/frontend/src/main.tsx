e79b1646b517c7013ca0621d74356b20edd86bcb
Ken Yun <yunhaen@gmail.com>
2025-04-16 11:04:38 -0400

    build: added react-router

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ClerkProvider } from '@clerk/clerk-react';
+import { BrowserRouter, Routes, Route } from 'react-router';
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -17,7 +18,9 @@ createRoot(document.getElementById('root')!).render(
       publishableKey={PUBLISHABLE_KEY}
       afterSignOutUrl='/'
     >
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ClerkProvider>
   </StrictMode>
 );
