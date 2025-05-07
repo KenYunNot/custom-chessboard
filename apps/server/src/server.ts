04a0e8d24787c6d83cda18fc80ae9bedd7f9edff
Ken Yun <yunhaen@gmail.com>
2025-05-07 09:59:18 -0400

    feat: rudimentary matchmaking

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -15,8 +15,23 @@ app.get('/', (_, res) => {
   res.sendFile(join(__dirname, '../', 'index.html'));
 });
 
-io.on('connection', (_) => {
-  console.log('A user connected!');
+io.on('connection', (socket) => {
+  console.log('A user has connected!');
+  socket.on('find-game', async ({ category, time, increment }) => {
+    // Fetch all sockets in pool
+    // If there are sockets, pick one to start game with
+    // Else, join the pool and wait
+    const pool = `${category}-${time}-${increment}`;
+    const sockets = await io.in(pool).fetchSockets();
+    if (sockets.length === 0) {
+      socket.join(pool);
+    } else {
+      const opponentSocket = sockets[Math.floor(Math.random() * sockets.length)];
+      const gameId = Math.ceil(Math.random() * 1000000);
+      opponentSocket.emit('play-game', gameId);
+      socket.emit('play-game', gameId);
+    }
+  });
 });
 
 server.listen(3000, () => {
