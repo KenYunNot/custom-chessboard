cfaad846becfd4b6e126f64e8717cb9e6573caa1
Ken Yun <yunhaen@gmail.com>
2025-05-08 14:58:43 -0400

    feat: extend socket with custom properties

--- apps/frontend/src/lib/socket.ts
+++ apps/frontend/src/lib/socket.ts
@@ -1,7 +1,15 @@
 import { io } from 'socket.io-client';
+import type { TimeControl } from './types';
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
+
+declare module 'socket.io-client' {
+  interface Socket {
+    gameId: string;
+    timeControl: TimeControl;
+  }
+}
 
 export const socket = io(URL, {
   autoConnect: false,
