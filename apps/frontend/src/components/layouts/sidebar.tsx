7e42004f9577bbd8c24f3354f759ff2d9146b1e3
Ken Yun <yunhaen@gmail.com>
2025-05-09 14:39:21 -0400

    feat: add play route link to sidebar

--- apps/frontend/src/components/layouts/sidebar.tsx
+++ apps/frontend/src/components/layouts/sidebar.tsx
@@ -3,9 +3,15 @@ import { Outlet } from 'react-router';
 
 const SidebarLayout = () => {
   return (
-    <div className='flex'>
-      <div className='w-32 p-1 flex flex-col bg-body-foreground text-white'>
+    <div className='h-screen flex'>
+      <div className='w-32 p-1 flex flex-col gap-1 bg-body-foreground text-white'>
         <SignedIn>
+          <a
+            href='/play'
+            className='py-2 bg-accent-primary rounded-md text-sm font-semibold text-center hover:brightness-110 hover:cursor-pointer'
+          >
+            Play
+          </a>
           <SignOutButton>
             <button className='py-2 bg-neutral-700 rounded-md text-sm font-semibold hover:bg-neutral-600 hover:cursor-pointer'>
               Sign Out
