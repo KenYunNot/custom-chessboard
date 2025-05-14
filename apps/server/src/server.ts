0567792b1d619835ace732f8fdb75f4531950fa6
Ken Yun <yunhaen@gmail.com>
2025-05-14 15:06:09 -0400

    feat: server side events for matchmaking and gameplay

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -2,35 +2,91 @@ import express from 'express';
 import { createServer } from 'node:http';
 import { join } from 'node:path';
 import { Server } from 'socket.io';
+import { GameStore } from './gameStore';
+import { type Move, Chess, DEFAULT_POSITION } from 'chess.js';
+
+declare module 'socket.io' {
+  interface Socket {
+    userId: string;
+    gameId: string;
+  }
+}
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'http://localhost:4173'],
+    origin: ['http://localhost:4173', 'http://localhost:5173'],
   },
 });
+const gameStore = new GameStore();
 
 app.get('/', (_, res) => {
   res.sendFile(join(__dirname, '../', 'index.html'));
 });
 
+io.use((socket, next) => {
+  const { userId, gameId } = socket.handshake.auth;
+  if (!userId) {
+    return next(new Error('Must be authenticated.'));
+  }
+  socket.userId = userId;
+  next();
+});
+
 io.on('connection', (socket) => {
   console.log('A user has connected!');
-  socket.on('find-game', async ({ category, time, increment }) => {
-    // Fetch all sockets in pool
-    // If there are sockets, pick one to start game with
-    // Else, join the pool and wait
-    const pool = `${category}-${time}-${increment}`;
-    const sockets = await io.in(pool).fetchSockets();
-    if (sockets.length === 0) {
-      socket.join(pool);
-    } else {
-      const opponentSocket = sockets[Math.floor(Math.random() * sockets.length)];
-      const gameId = Math.ceil(Math.random() * 1000000);
-      opponentSocket.emit('play-game', gameId);
-      socket.emit('play-game', gameId);
+
+  socket.on(
+    'find-opponent',
+    async (timeControl: { category: string; time: number; increment: number }) => {
+      // Guard against users who already have gameId set
+      const { category, time, increment } = timeControl;
+      const pool = `${category}-${time}-${increment}`;
+      const sockets = await io.in(pool).fetchSockets();
+      if (sockets.length === 0) {
+        socket.join(pool);
+      } else {
+        const gameId = Math.ceil(Math.random() * 10000000).toString();
+        const opponentSocket = sockets[Math.floor(Math.random() * sockets.length)];
+        const [playerWhiteSocket, playerBlackSocket] =
+          Math.random() < 0.5 ? [socket, opponentSocket] : [opponentSocket, socket];
+        gameStore.saveGame(gameId, {
+          id: gameId,
+          fen: DEFAULT_POSITION,
+          playerWhiteId: playerWhiteSocket.id,
+          playerBlackId: playerBlackSocket.id,
+        });
+        playerWhiteSocket.join(gameId);
+        playerBlackSocket.join(gameId);
+        playerWhiteSocket.emit('found-opponent', gameId, 'w', playerBlackSocket.id);
+        playerBlackSocket.emit('found-opponent', gameId, 'b', playerWhiteSocket.id);
+      }
     }
+  );
+
+  socket.on('play-game', (gameId: string) => {
+    socket.on('move', (move: Move) => {
+      const { fen, playerWhiteId, playerBlackId } = gameStore.findGame(gameId);
+      const chess = new Chess(fen);
+      if (chess.turn() === 'w' && socket.id !== playerWhiteId) return;
+      if (chess.turn() === 'b' && socket.id !== playerBlackId) return;
+      try {
+        chess.move({
+          from: move.from,
+          to: move.to,
+        });
+        gameStore.updateFen(gameId, chess.fen());
+        socket.to(gameId).emit('move', move);
+      } catch (error) {
+        socket.emit('invalid-move', fen);
+      }
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user has disconnected!');
+    gameStore.removeGame(socket.gameId);
   });
 });
 
