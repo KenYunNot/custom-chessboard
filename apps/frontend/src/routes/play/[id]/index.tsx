441d7f82e81f62f87fc49a1f84e5975875126396
Ken Yun <yunhaen@gmail.com>
2025-05-15 15:38:41 -0400

    feat: added game over dialog

--- apps/frontend/src/routes/play/[id]/index.tsx
+++ apps/frontend/src/routes/play/[id]/index.tsx
@@ -4,12 +4,15 @@ import { useNavigate, useParams } from 'react-router';
 import Board from '@/components/board';
 import { Chess, type Move, type Square } from 'chess.js';
 import { toast } from 'sonner';
+import { match } from 'ts-pattern';
 
 const PlayGame = () => {
   const navigate = useNavigate();
   const { gameId } = useParams();
   const chess = React.useMemo(() => new Chess(), []);
   const [fen, setFen] = React.useState(chess.fen());
+  const [isGameOver, setIsGameOver] = React.useState<boolean>(false);
+  const [popupContent, setPopupContent] = React.useState<{ title: string; message: string } | null>(null);
 
   React.useEffect(() => {
     if (!socket.gameId) {
@@ -34,6 +37,28 @@ const PlayGame = () => {
       chess.load(oldFen);
       toast.error('Invalid move!');
     });
+    socket.on('checkmate', (isWinner: boolean) => {
+      setPopupContent({
+        title: 'Game Over',
+        message: isWinner ? 'You won by checkmate!' : 'Opponent won by checkmate.',
+      });
+      setIsGameOver(true);
+    });
+    socket.on(
+      'draw',
+      (results: { stalemate: boolean; repetition: boolean; fiftyMoveRule: boolean; insufficientMaterial: boolean }) => {
+        const message = match(results)
+          .with({ stalemate: true }, () => 'by stalemate.')
+          .with({ repetition: true }, () => 'by three-fold repetition.')
+          .with({ fiftyMoveRule: true }, () => 'by fifty move rule.')
+          .with({ insufficientMaterial: true }, () => 'by insufficient material.')
+          .otherwise(() => 'by game rule.');
+        setPopupContent({
+          title: 'Draw',
+          message,
+        });
+      }
+    );
 
     return () => {
       socket.off('move');
@@ -56,11 +81,19 @@ const PlayGame = () => {
 
   return (
     <div className='w-full h-full'>
-      <Board
-        fen={fen}
-        orientation={socket.color}
-        onDrop={onDrop}
-      />
+      <div className='w-auto h-full relative'>
+        {isGameOver && (
+          <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-fit h-fit p-5 flex flex-col items-center justify-center gap-3 bg-body-foreground text-white rounded-md z-50'>
+            <p className='text-2xl font-bold'>{popupContent?.title}</p>
+            {popupContent?.message}
+          </div>
+        )}
+        <Board
+          fen={fen}
+          orientation={socket.color}
+          onDrop={onDrop}
+        />
+      </div>
     </div>
   );
 };
