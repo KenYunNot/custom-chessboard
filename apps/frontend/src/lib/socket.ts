04fdc2e3909abc4e94bcce431b9c602aff733836
Ken Yun <yunhaen@gmail.com>
2025-05-20 11:50:56 -0400

    fix: remove unused attributes from socket

--- apps/frontend/src/lib/socket.ts
+++ apps/frontend/src/lib/socket.ts
@@ -1,14 +1,11 @@
 import { io } from 'socket.io-client';
 import type { TimeControl } from './types';
-import type { Color } from 'chess.js';
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
 
 declare module 'socket.io-client' {
   interface Socket {
-    gameId: string;
-    color: Color;
     timeControl: TimeControl;
   }
 }
