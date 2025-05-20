8aba9a926a9877d5915787bb96ee43c2af7a3c77
Ken Yun <yunhaen@gmail.com>
2025-05-20 11:51:51 -0400

    feat: handle session restoration

--- apps/frontend/src/routes/play/[id]/index.tsx
+++ apps/frontend/src/routes/play/[id]/index.tsx
@@ -2,73 +2,129 @@ import React from 'react';
 import { socket } from '@/lib/socket';
 import { useNavigate, useParams } from 'react-router';
 import Board from '@/components/board';
-import { Chess, type Move, type Square } from 'chess.js';
+import { Chess, type Color, type Move, type Square } from 'chess.js';
 import { toast } from 'sonner';
 import { match } from 'ts-pattern';
+import { useUser } from '@clerk/clerk-react';
 
 const PlayGame = () => {
   const navigate = useNavigate();
   const { gameId } = useParams();
+  const { user } = useUser();
+  const [isConnected, setIsConnected] = React.useState<boolean>(socket.connected);
   const chess = React.useMemo(() => new Chess(), []);
   const [fen, setFen] = React.useState(chess.fen());
+  const [color, setColor] = React.useState<Color>();
+  const [opponentId, setOpponentId] = React.useState<string>();
   const [isGameOver, setIsGameOver] = React.useState<boolean>(false);
   const [popupContent, setPopupContent] = React.useState<{ title: string; message: string } | null>(null);
 
   React.useEffect(() => {
-    if (!socket.gameId) {
+    if (!user) return;
+
+    const sessionId = localStorage.getItem('sessionId');
+    if (sessionId) {
+      socket.auth = { userId: user?.id, sessionId };
+      socket.connect();
+    } else {
       navigate('/play');
-      return;
-    } else if (socket.gameId !== gameId) {
-      navigate(`/play/${socket.gameId}`);
-      return;
     }
+
+    const onConnect = () => {
+      setIsConnected(true);
+    };
+
+    const onConnectError = (error: Error) => {
+      toast.error(error.message);
+      localStorage.removeItem('sessionId');
+      navigate('/play');
+    };
+
+    socket.on('connect', onConnect);
+    socket.on('connect_error', onConnectError);
+
+    return () => {
+      socket.off('connect');
+      socket.off('connect_error');
+    };
+  }, [user]);
+
+  React.useEffect(() => {
+    if (!isConnected) return;
 
     socket.emit('play-game', gameId);
 
-    socket.on('move', (move: Move) => {
+    const onAlreadyInGame = (gameId: string) => {
+      navigate(`/play/${gameId}`);
+    };
+
+    const onSession = ({ color, opponentId, fen }: { color: Color; opponentId: string; fen: string }) => {
+      chess.load(fen);
+      setFen(chess.fen());
+      setColor(color);
+      setOpponentId(opponentId);
+    };
+
+    const onMove = (move: Move) => {
       console.log('Got the move!');
       chess.move({
         from: move.from,
         to: move.to,
       });
       setFen(chess.fen());
-    });
-    socket.on('invalid-move', (oldFen: string) => {
+    };
+
+    const onInvalidMove = (oldFen: string) => {
       chess.load(oldFen);
       toast.error('Invalid move!');
-    });
-    socket.on('checkmate', (isWinner: boolean) => {
+    };
+
+    const onCheckmate = (isWinner: boolean) => {
       setPopupContent({
         title: 'Game Over',
         message: isWinner ? 'You won by checkmate!' : 'Opponent won by checkmate.',
       });
       setIsGameOver(true);
-    });
-    socket.on(
-      'draw',
-      (results: { stalemate: boolean; repetition: boolean; fiftyMoveRule: boolean; insufficientMaterial: boolean }) => {
-        const message = match(results)
-          .with({ stalemate: true }, () => 'by stalemate.')
-          .with({ repetition: true }, () => 'by three-fold repetition.')
-          .with({ fiftyMoveRule: true }, () => 'by fifty move rule.')
-          .with({ insufficientMaterial: true }, () => 'by insufficient material.')
-          .otherwise(() => 'by game rule.');
-        setPopupContent({
-          title: 'Draw',
-          message,
-        });
-      }
-    );
+    };
+
+    const onDraw = (results: {
+      stalemate: boolean;
+      repetition: boolean;
+      fiftyMoveRule: boolean;
+      insufficientMaterial: boolean;
+    }) => {
+      const message = match(results)
+        .with({ stalemate: true }, () => 'by stalemate.')
+        .with({ repetition: true }, () => 'by three-fold repetition.')
+        .with({ fiftyMoveRule: true }, () => 'by fifty move rule.')
+        .with({ insufficientMaterial: true }, () => 'by insufficient material.')
+        .otherwise(() => 'by game rule.');
+      setPopupContent({
+        title: 'Draw',
+        message,
+      });
+    };
+
+    socket.on('session', onSession);
+    socket.on('already-in-game', onAlreadyInGame);
+    socket.on('move', onMove);
+    socket.on('invalid-move', onInvalidMove);
+    socket.on('checkmate', onCheckmate);
+    socket.on('draw', onDraw);
 
     return () => {
+      socket.off('session');
+      socket.off('already-in-game');
       socket.off('move');
       socket.off('invalid-move');
+      socket.off('checkmate');
+      socket.off('draw');
     };
-  }, []);
+  }, [isConnected]);
 
   const onDrop = (from: Square, to: Square) => {
     if (from === to) return;
-    if (chess.turn() !== socket.color) return;
+    if (chess.turn() !== color) return;
     try {
       const move = chess.move({
         from,
@@ -76,7 +132,9 @@ const PlayGame = () => {
       });
       setFen(chess.fen());
       socket.emit('move', move);
-    } catch (error) {}
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -90,7 +148,7 @@ const PlayGame = () => {
         )}
         <Board
           fen={fen}
-          orientation={socket.color}
+          orientation={color}
           onDrop={onDrop}
         />
       </div>
